b36c2dea7427025bbe4481ab90ebc7a1b4d1f593
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-06-24 17:26:43 +0530

    chore: minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./global.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { RootProvider } from "fumadocs-ui/provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -64,6 +65,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <GoogleAnalytics gaId="G-950MSN3L0J" />
       </head>
       <body className="flex flex-col min-h-screen selection:bg-[#79ffe1] dark:selection:bg-[#f81ce5] dark:selection:text-white selection:text-secondary-700">
         <RootProvider>{children}</RootProvider>
